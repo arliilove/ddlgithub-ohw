d3a4a3e08f5dcb8bbcd987c6bda2ea308d368acd
arliilove <arli@sjtu.edu.cn>
2018-10-23 14:48:29 +0800

    练习用python读取minecraft玩家位置

--- 2018autumn/welcomehome.py
+++ 2018autumn/welcomehome.py
@@ -1,8 +1,9 @@
 from mcpi.minecraft import Minecraft
 import time
+import mcpi.block as block
 
 mc=Minecraft.create()
-#mc=Minecraft.create("10.163.80.195",4711)
+
 
 stayed_time=0
 
@@ -12,7 +13,7 @@ while True:
     pos=mc.player.getTilePos()
     mc.postToChat("please goto home x=-30 y=-6 z=-40 for 15s to fly")
     mc.postToChat("x:"+str(pos.x)+"y:"+str(pos.y)+"z:"+str(pos.z)) 
-    if pos.x==33 and pos.y==16 and pos.z==164:
+    if pos.x==-62 and pos.y==-4 and pos.z==-41:
         mc.postToChat("welcome home")
         stayed_time=stayed_time+1
         if stayed_time>=30:
